73847e90bc0db9d8bebe418548be549374b86bf3
Sharvesh <sharvesh.somasundaram@kalvium.community>
2023-05-11 08:38:02 +0530

    Finished my Home Page

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -1,15 +1,12 @@
 import { Flex, Text, Heading, ChakraProvider } from '@chakra-ui/react'
 import customTheme from './chakraTheme'
+import Navbar from './components/navbar_homepage'
+import Bg from './components/HomePage'
 
 function App() {
     return (
         <ChakraProvider theme={customTheme}>
-            <Flex bg="white.400" h="100vh">
-                <Heading as="h1" size="4xl">
-                    Grievance Go
-                </Heading>
-                <Text>A platform where your complaints shall be solved</Text>
-            </Flex>
+            <Bg />
         </ChakraProvider>
     )
 }
--- Front-end/src/main.jsx
+++ Front-end/src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+// import './index.css'
 import { ChakraProvider } from '@chakra-ui/react'
 import customTheme from './chakraTheme'
+import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
